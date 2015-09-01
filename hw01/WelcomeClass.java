2a60f7f54f470fe92abd882080ca2ed7300353aa
ale219 <ale219@lehigh.edu>
2015-09-01 21:04:27 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,23 @@
+  
+  
+  
+  public class WelcomeClass{
+  
+  
+    public static void main(String[] args) {
+  
+  System.out.println( "  -----------");
+  System.out.println( "  | WELCOME |"); //Begin with introduction
+   System.out.println("  -----------");
+     System.out.println("  ^  ^  ^  ^  ^  ^");
+ System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ "); //Design for aesthetic appeal
+System.out.println("<-A--L--E--2--1--0->"); //Lehigh ID
+ System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+ System.out.println("  v  v  v  v  v  v");
+System.out.println("I am 17 years old. I attend Lehigh University. I am in the P.C. Rossin College of Engineering."); //Quick autobiographical statement
+
+
+
+    }
+}
+
